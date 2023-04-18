a055838cec54e21dd9ec1405d96a0d2ad597c8d1
carsonology <carson.terbush@gmail.com>
2023-04-18 14:16:14 -0400

    clean location cols

--- workflow/cleaning-scripts/main_data_cleaning.py
+++ workflow/cleaning-scripts/main_data_cleaning.py
@@ -4,13 +4,13 @@ input:
     - moco-crash-2013-2018.csv
     - moco-crash-2003-2015.csv
 output:
-    - data-output/standard-fields/moco-crash-2013-2018.csv
-    - data-output/standard-fields/moco-crash-2003-2015.csv
+    - data-output/temp/moco-crash-2013-2018.csv
+    - data-output/temp/moco-crash-2003-2015.csv
 """
 import sys 
 
 import pandas as pd
-import numpy as np
+import re
 
 def load_data(crash_file):
     """ returns a pandas dataframe of the raw dataset. """
@@ -19,6 +19,32 @@ def load_data(crash_file):
 def rename_df(crash_df, rename_dict):
     """ renaming columns to match with 2019-2022 field names """
     return crash_df.rename(index=str, columns=rename_dict, inplace=False)
+
+def get_road1(row):
+    if isinstance(row, str):
+        return row.split("&")[0].strip()
+    else:
+        return ''
+def get_road2(row):
+    if isinstance(row,str):
+        return row.split("&")[1] if len(row.split("&")) > 1 else ''
+        
+    else:
+        return ''
+# remove all info in parentheses in street names
+def remove_parens(row):
+    return " ".join(re.sub('\(.*\)', '', row).split())
+
+def organize_location_cols(df):
+    df['Roadway Id'] = df['Reported_Location'].apply(get_road1)
+    df['Intersecting Road'] = df['Reported_Location'].apply(get_road2)
+
+    df['Roadway Id'] = df['Roadway Id'].fillna('')
+    df['Intersecting Road'] = df['Intersecting Road'].fillna('')
+
+    df['Roadway Id'] = df['Roadway Id'].apply(remove_parens)
+    df['Intersecting Road'] = df['Intersecting Road'].apply(remove_parens)
+    return df.drop(columns=["Reported_Location"])
 
 def save_clean_df(cleaned_df, out_file):
     ''' save the cleaned df '''
@@ -59,8 +85,10 @@ if __name__ == "__main__":
     OUTFILE_13_18 = sys.argv[3]
     OUTFILE_03_15 = sys.argv[4]
 
+    # print(organize_location_cols(rename_df(DF_03_15,rename_dict_03_15)))
+
     CLEAN_DF_13_18 = rename_df(DF_13_18,rename_dict_13_18)
-    CLEAN_DF_03_15 = rename_df(DF_03_15,rename_dict_03_15)
+    CLEAN_DF_03_15 = organize_location_cols(rename_df(DF_03_15,rename_dict_03_15))
 
     save_clean_df(CLEAN_DF_13_18,OUTFILE_13_18)
     save_clean_df(CLEAN_DF_03_15,OUTFILE_03_15)
